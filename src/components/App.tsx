544380c13a1928449a2a605df7c5ed61861b248c
hassan <hassan.umair@gmail.com>
2020-05-11 21:23:39 +0200

    feat(clean): clean up

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -81,8 +81,8 @@ const App: FunctionComponent<AppProps> = ({ widget }) => {
 
   return (
     <div className="font-robo text-white bg-weather-bg min-h-screen py-28">
-      <div className="flex justify-between px-20 mb-20">
-        <div className="w-64">
+      <div className="flex flex-col sm:flex-row justify-between px-20 mb-20">
+        <div className="w-32 md:w-48">
           <svg
             className="fill-current text-weather-yellow"
             xmlns="http://www.w3.org/2000/svg"
@@ -91,9 +91,9 @@ const App: FunctionComponent<AppProps> = ({ widget }) => {
             <path d="M12 19.5c-4.135 0-7.5-3.365-7.5-7.5S7.865 4.5 12 4.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5zM12 6c-3.308 0-6 2.692-6 6s2.692 6 6 6 6-2.692 6-6-2.692-6-6-6zM12 3a.75.75 0 01-.75-.75V.75a.75.75 0 011.5 0v1.5A.75.75 0 0112 3zM18.894 5.856a.743.743 0 01-.53-.22.744.744 0 010-1.06l1.061-1.061a.744.744 0 011.06 0 .744.744 0 010 1.06l-1.061 1.061a.743.743 0 01-.53.22zM21.75 12.75a.75.75 0 010-1.5h1.5a.75.75 0 010 1.5h-1.5zM19.955 20.705a.743.743 0 01-.53-.22l-1.061-1.061a.752.752 0 01.53-1.281c.2 0 .389.078.53.22l1.061 1.061a.752.752 0 01-.53 1.281zM12 24a.75.75 0 01-.75-.75v-1.5a.75.75 0 011.5 0v1.5A.75.75 0 0112 24zM4.045 20.705a.743.743 0 01-.53-.22.744.744 0 010-1.06l1.061-1.061c.141-.142.33-.22.53-.22s.389.078.53.22a.752.752 0 010 1.061l-1.061 1.061a.744.744 0 01-.53.219zM.75 12.75a.75.75 0 010-1.5h1.5a.75.75 0 010 1.5H.75zM5.106 5.856a.743.743 0 01-.53-.22L3.515 4.575c-.142-.141-.22-.33-.22-.53s.078-.389.22-.53c.141-.142.33-.22.53-.22s.389.078.53.22l1.061 1.061c.142.141.22.33.22.53s-.078.389-.22.53a.743.743 0 01-.53.22z" />
           </svg>
         </div>
-        <div className="w-112">
+        <div className="hidden sm:block sm:w-112">
           <div className="flex justify-around text-normal">
-            <span className="text-weather-grey text-4xl text-left w-50 capitalize">
+            <span className="flex text-weather-grey text-4xl text-left w-50 capitalize">
               {currentDay.weather || 'meatballs'}
             </span>
             <span className="text-weather-grey text-4xl text-right w-50">
@@ -107,9 +107,11 @@ const App: FunctionComponent<AppProps> = ({ widget }) => {
             </span>
           </div>
         </div>
-        <div className="w-date">
-          <div className="text-weather-grey text-4xl w-50">{cityName}</div>
-          <div className="text-white text-lg font-bold pt-8">
+        <div className="sm:w-date">
+          <div className="text-center sm:text-left text-weather-grey text-4xl w-50">
+            {cityName}
+          </div>
+          <div className="text-white text-3xl sm:text-lg font-bold pt-8">
             {currentDay.date}
           </div>
         </div>
