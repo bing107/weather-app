0076affd8dcecccdb3d94178b19fce1e5e218aa0
hassan <hassan.umair@gmail.com>
2020-05-11 16:32:15 +0200

    chore(api): css mock for jest

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,7 +7,8 @@ const App = () => {
     const fetchLocationProperties = async () => {
       try {
         const results = await fetch(
-          'https://samples.openweathermap.org/data/2.5/forecast?q=M%C3%BCnchen,DE&appid=b6907d289e10d714a6e88b30761fae22'
+          'https://samples.openweathermap.org/data/2.5/forecast?q=M%C3%BCnchen,DE&appid=b6907d289e10d714a6e88b30761fae22',
+          { mode: 'no-cors' }
         );
         // const { items } = await results.json();
         console.log(results);
